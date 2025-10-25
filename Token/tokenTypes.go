d332e9a612029cb5acf2b15fb5776d912a1534f5
GarethSimmans <gareth.simmans@gmail.com>
2025-10-25 12:09:05 +0100

    TypesEnum

---
+++ Token/tokenTypes.go
@@ -0,0 +1,20 @@
+package Token
+
+import "fmt"
+
+type TokenType int
+
+
+var TokenID = map[TokenType]string{
+	//primitives
+	byte: "byte"
+	
+	//operations
+
+	//io
+
+	//functions
+	__func: "func",
+
+	//array
+}
