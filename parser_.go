2c62b91194b49ca5de1ed1a9b3f2b2fa45b3d9b8
GarethSimmans <gareth.simmans@gmail.com>
2025-10-25 16:37:50 +0100

    ldv

--- parser_.go
+++ parser_.go
@@ -1,4 +1,70 @@
 package main
 
-func ParseData(data []byte) {
+var mappings map[string]byte
+
+func ParseData(Tokens []__TokenType) {
+
+	var idx = 0;
+	loop {
+		if idx >= Tokens.len {
+			break
+		}
+		switch(Tokens[idx]) 
+		case LDV:
+			fmt.Println("LDV")
+			mappings[Tokens[idx + 2]], err := byte(strconv.ParseUint(Tokens[idx + 3]))
+			idx += 4	
+			break
+		case PLUS:
+			fmt.Println("PLUS")
+			break
+		case MINUS:
+			fmt.Println("SULP or MINUS")
+			break
+		case EQUAL_EQUAL:
+			fmt.Println("EQUAL_EQUAL")
+			break
+		case GREATER_THAN:
+			fmt.Println("GREATER_THAN")
+			break
+		case LESS_THAN:
+			fmt.Println("LESS_THAN")
+			break
+		case READ:
+			fmt.Println("READ")
+			break
+		case PRINT:
+			fmt.Println("PRINT")
+			break
+		default:
+			fmt.Println("Unknown TokenType")
+		}
+	}
+	case LDV:
+		fmt.Println("LDV")
+		break
+	case PLUS:
+		fmt.Println("PLUS")
+		break
+	case MINUS:
+		fmt.Println("SULP or MINUS")
+		break
+	case EQUAL_EQUAL:
+		fmt.Println("EQUAL_EQUAL")
+		break
+	case GREATER_THAN:
+		fmt.Println("GREATER_THAN")
+		break
+	case LESS_THAN:
+		fmt.Println("LESS_THAN")
+		break
+	case READ:
+		fmt.Println("READ")
+		break
+	case PRINT:
+		fmt.Println("PRINT")
+		break
+	default:
+		fmt.Println("Unknown TokenType")
+	}
 }
