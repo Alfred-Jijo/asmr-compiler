073a402473f6f975df33ee4a31cd099ea462ab96
Alfred <alfredjijo28+goland_github@gmail.com>
2025-10-25 15:53:32 +0100

    Code Cleanup

--- lexer_.go
+++ lexer_.go
@@ -10,7 +10,6 @@ var Keywords = make(map[string]__TokenType)
 
 func Lex(data []byte) {
 	line := string(data)
-	// lines := strings.Split(line, " ")
 	lines := splitMDelim(line, " \n")
 
 	/*if DEBUG {
@@ -37,70 +36,6 @@ func Lex(data []byte) {
 		}
 	}
 	fmt.Println(tokens)
-}
-
-func printEnumVal(toktype __TokenType) {
-	switch toktype {
-	case BYTE:
-		fmt.Println("BYTE")
-		break
-	case BYTED8:
-		fmt.Println("BYTED8")
-		break
-	case LDV:
-		fmt.Println("LDV")
-		break
-	case BEGIN:
-		fmt.Println("BEGIN")
-		break
-	case END:
-		fmt.Println("END")
-		break
-	case PLUS:
-		fmt.Println("PLUS")
-		break
-	case MINUS:
-		fmt.Println("SULP or MINUS")
-		break
-	case EQUAL_EQUAL:
-		fmt.Println("EQUAL_EQUAL")
-		break
-	case GREATER_THAN:
-		fmt.Println("GREATER_THAN")
-		break
-	case LESS_THAN:
-		fmt.Println("LESS_THAN")
-		break
-	case NOT:
-		fmt.Println("NOT")
-		break
-	case READ:
-		fmt.Println("READ")
-		break
-	case PRINT:
-		fmt.Println("PRINT")
-		break
-	case DECL:
-		fmt.Println("DECL")
-		break
-	case CALL:
-		fmt.Println("CALL")
-		break
-	case BRANCH:
-		fmt.Println("BRANCH")
-		break
-	case LIST:
-		fmt.Println("LIST")
-		break
-	case END_LIST:
-		fmt.Println("END_LIST")
-		break
-	case NONE:
-		fmt.Println("NONE")
-		break
-	default:
-		fmt.Println("Unknown TokenType")
-	}
 }
 
 func populateRmap() {
--- token_.go
+++ token_.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type __TokenType int
 
 const (
@@ -62,4 +64,68 @@ var TokenID = map[__TokenType]string{
 type __Token struct {
 	__tokenType __TokenType
 	lexeme      string
+}
+
+func printEnumVal(toktype __TokenType) {
+	switch toktype {
+	case BYTE:
+		fmt.Println("BYTE")
+		break
+	case BYTED8:
+		fmt.Println("BYTED8")
+		break
+	case LDV:
+		fmt.Println("LDV")
+		break
+	case BEGIN:
+		fmt.Println("BEGIN")
+		break
+	case END:
+		fmt.Println("END")
+		break
+	case PLUS:
+		fmt.Println("PLUS")
+		break
+	case MINUS:
+		fmt.Println("SULP or MINUS")
+		break
+	case EQUAL_EQUAL:
+		fmt.Println("EQUAL_EQUAL")
+		break
+	case GREATER_THAN:
+		fmt.Println("GREATER_THAN")
+		break
+	case LESS_THAN:
+		fmt.Println("LESS_THAN")
+		break
+	case NOT:
+		fmt.Println("NOT")
+		break
+	case READ:
+		fmt.Println("READ")
+		break
+	case PRINT:
+		fmt.Println("PRINT")
+		break
+	case DECL:
+		fmt.Println("DECL")
+		break
+	case CALL:
+		fmt.Println("CALL")
+		break
+	case BRANCH:
+		fmt.Println("BRANCH")
+		break
+	case LIST:
+		fmt.Println("LIST")
+		break
+	case END_LIST:
+		fmt.Println("END_LIST")
+		break
+	case NONE:
+		fmt.Println("NONE")
+		break
+	default:
+		fmt.Println("Unknown TokenType")
+	}
 }
