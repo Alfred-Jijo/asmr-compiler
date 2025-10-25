ffd09aae824283488a6e8d36eed2a0a2040185b5
GarethSimmans <gareth.simmans@gmail.com>
2025-10-25 15:08:50 +0100

    fix

--- lexer_.go
+++ lexer_.go
@@ -9,9 +9,10 @@ func Lex(data []byte) {
 	line := string(data)
 	lines := strings.Split(line, " ")
 
-	tokens []_token
+	tokens []__Token;
 	for _, line := range lines{
-		tokens.append(_token{type:  ,lexeme: _})
+		
+		tokens.append(__Token{tokenType:  ,lexeme: _})
 	}
 
 	if DEBUG {
