b2cbf498280253f73d080762c5756545f9ac7af6
Alfred <alfredjijo28+goland_github@gmail.com>
2025-10-25 13:08:30 +0100

    test of fIO

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
+func CreateFile() {
+	fmt.Println("Writing file")
+	file, err := os.Create("test.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	length, err := file.WriteString("Welcome to Golang. This demonstrates file operations.")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("File name: %s\n", file.Name())
+	fmt.Printf("File length: %d bytes\n", length)
+}
 func main() {
-	fmt.Println("Hello World")
+
 }
